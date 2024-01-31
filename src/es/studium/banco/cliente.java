ea144de37a31116dd92be8b887f576d0374aaeb5
peich16 <158155756+peich16@users.noreply.github.com>
2024-01-31 14:18:42 +0100

    Update cliente.java

--- src/es/studium/banco/cliente.java
+++ src/es/studium/banco/cliente.java
@@ -1,14 +1,26 @@
 package es.studium.banco;
-
+/**
+ ** Clase POJO cliente
+ * Clase que genera a los clientes
+ * @author Pablo Fernández De Los Ríos Mesa
+ * @version v3
+ */
 public class cliente
 {
 // Aquí van los atributos
+	
+	/**
+	 * Son los elementos que definen la clase cliente.
+	 */
 	private String dni;
 	private String nombre;
 	private String direccion;
 	
 	
 // Pongo el constructor vacío
+	/**
+	 * Constructor por defecto
+	 */
 	
 	public cliente()
 	{
@@ -16,7 +28,14 @@ public class cliente
 		nombre = "";
 		direccion = "";
 	}
-	// El constructor por parámetros (constructor por campos y métodos get and set)
+	// El constructor por parámetros (constructor por campos y metodos get and set)
+	
+	/**
+	 * Constructor con todos los parámetros
+	 * @param dni Se asigna al atributo dni
+	 * @param nombre Se asigna al atributo nombre
+	 * @param dirección Se asigna al atributo direccion
+	 */
 	
 
 public cliente(String dni, String nombre, String dirección)
@@ -25,26 +44,56 @@ public cliente(String dni, String nombre, String dirección)
 	this.nombre = nombre;
 	this.direccion = dirección;
 }
+
+/**
+ * Metodo get del atributo dni
+ * @return Devuelve el valor del atributo dni
+ */
+
 public String getDni()
 {
 	return dni;
 }
+
+/**
+ * Metodo set del atributo dni
+ * @param dni Se asigna al atributo dni
+ */
 public void setDni(String dni)
 {
 	this.dni = dni;
 }
+/**
+ * Metodo get del atributo nombre
+ * @return Devuelve el valor del atributo nombre
+ */
 public String getNombre()
 {
 	return nombre;
 }
+
+/**
+ * Metodo set del atributo nombre
+ * @param nombre Devuelve el valor del atributo nombre
+ */
 public void setNombre(String nombre)
 {
 	this.nombre = nombre;
 }
+
+/**
+ * Metodo get del atributo direccion
+ * @return Devuelve el valor del atributo direccion
+ */
 public String getDireccion()
 {
 	return direccion;
 }
+
+/**
+ * Metodo set del atributo direccion
+ * @param direccion Devuelve el valor del atributo direccion
+ */
 public void setDireccion(String direccion)
 {
 	this.direccion = direccion;
