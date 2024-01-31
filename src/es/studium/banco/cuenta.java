8d76d37e32d7586eb13f943fc9a7467c9d432850
peich16 <158155756+peich16@users.noreply.github.com>
2024-01-31 12:53:42 +0100

    Update cuenta.java

--- src/es/studium/banco/cuenta.java
+++ src/es/studium/banco/cuenta.java
@@ -2,61 +2,64 @@ package es.studium.banco;
 
 public class cuenta
 {
-//Los atributos
-	
-	public int tipo;
-	public int saldo;
-	private cliente cliente;
-	
-//Constructor vacío
-	
-	public cuenta()
-	{
-		tipo= 0;
-		saldo= 0;
-		cliente = new cliente();
-	}
-	
-	//El constructor por parámetros (métodos get y set)
-	
+    //Los atributos
+    public int tipo;
+    public int saldo;
+    private cliente cliente;
 
-	public cuenta(int tipo, int saldo, es.studium.banco.cliente cliente)
-	{
-		this.tipo = tipo;
-		this.saldo = saldo;
-		this.cliente = cliente;
-		
-		
-	}
+    //Constructor vacío
+    public cuenta()
+    {
+        tipo= 0;
+        saldo= 0;
+        cliente = new cliente();
+    }
 
-	public int getTipo()
-	{
-		return tipo;
-	}
+    //El constructor por parámetros (métodos get y set)
+    public cuenta(int tipo, int saldo, es.studium.banco.cliente cliente)
+    {
+        this.tipo = tipo;
+        this.saldo = saldo;
+        this.cliente = cliente;
+    }
 
-	public void setTipo(int tipo)
-	{
-		this.tipo = tipo;
-	}
+    public int getTipo()
+    {
+        return tipo;
+    }
 
-	public int getSaldo()
-	{
-		return saldo;
-	}
+    public void setTipo(int tipo)
+    {
+        this.tipo = tipo;
+    }
 
-	public void setSaldo(int saldo)
-	{
-		this.saldo = saldo;
-	}
+    public int getSaldo()
+    {
+        return saldo;
+    }
 
-	public cliente getCliente()
-	{
-		return cliente;
-	}
+    public void setSaldo(int saldo)
+    {
+        this.saldo = saldo;
+    }
 
-	public void setCliente(cliente cliente)
-	{
-		this.cliente = cliente;
-	}
-	
+    public cliente getCliente()
+    {
+        return cliente;
+    }
+
+    public void setCliente(cliente cliente)
+    {
+        this.cliente = cliente;
+    }
+
+    // Método para ingresar dinero en la cuenta
+    public void ingresarDinero(int cantidad) {
+        this.saldo += cantidad;
+    }
+
+    // Método para retirar dinero de la cuenta
+    public void retirarDinero(int cantidad) {
+        this.saldo -= cantidad;
+    }
 }
